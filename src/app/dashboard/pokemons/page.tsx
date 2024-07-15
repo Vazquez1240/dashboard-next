fb07f47e243cc3aea315d899c65087fe68ac9806
martin <vazquezmartin1240@gmail.com>
2024-07-15 01:17:56 -0600

    visualizacion de pokemons en imagen, nobre y id

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -2,22 +2,34 @@
 
 import {getPokemons} from "@/app/pokemons";
 import {useState, useEffect} from "react";
+import Image from "next/image";
 
 export default function PokemonPage() {
     const [pokemon, setPokemon] = useState([]);
-
     useEffect(() => {
-    const fetchData = async () => {
-      const data = await getPokemons(24);
+   (async () => {
+      const data = await getPokemons(40);
       // @ts-ignore
-        setPokemon(data);
-    };
-
-    fetchData();
+       setPokemon(data);
+    })();
   }, []);
     return (
-        <div>
-            {JSON.stringify(pokemon)}
+        <div className='flex flex-col'>
+            <div className='flex flex-wrap gap-10 items-center justify-center'>
+                {pokemon.map(poke => (
+                    <>
+                        <div>
+                            <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${poke.id}.svg`}
+                                key={poke.id}
+                                width={100}
+                                height={100}
+                                alt={poke.name}/>
+                            <p>{poke.name}</p>
+                            <p>{poke.id}</p>
+                        </div>
+                    </>
+                ))}
+            </div>
         </div>
     )
 }
