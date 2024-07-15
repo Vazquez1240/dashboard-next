afdfdeb66678b2ae301270b36d05d9fd4ff61158
martin <vazquezmartin1240@gmail.com>
2024-07-14 23:36:58 -0600

    Nueva manera de hacer la peticion a la pokepie

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,10 +1,26 @@
+'use server'
+// 'use client'
 
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react"
 
-export default function PokemonPage() {
+const getPokemons = (limit = 20, offset = 0) => {
+    try {
+        const data = fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+            // .then(res => res.json())
+        return data
+        // return fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
+export default async function PokemonPage() {
+    'use client'
+    const pokemon =  await getPokemons()
+    {console.log(pokemon)}
     return (
         <div>
-            <h1>Hello Page Pokemon</h1>
+            <h1>{JSON.stringify(pokemon)}</h1>
         </div>
     )
 }
